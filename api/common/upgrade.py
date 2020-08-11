9825771a02f4cb80cbdfd1f5e7685dc5a3d37eb2
archerckk <archerckk@163.com>
2020-08-11 17:41:06 +0800

    优化token存放逻辑

--- api/common/upgrade.py
+++ api/common/upgrade.py
@@ -7,10 +7,15 @@ with open('../config/api_payload_data.yml', 'r')as f:
     data = yaml.safe_load(f)['get_User_Version_Upgrade']
     print(data)
 
+with open('../config/server_config.yml')as f:
+    token_data=yaml.safe_load(f)
+    token=token_data['token'][token_data['default']]
+
 class Upgrade(BaseApi):
 
     def get_User_Version_Upgrade(self,version_code,lang):
         data['json']['version_code']=version_code
         data['json']['lang']=lang
+        data['headers']['Authorization']=token
         res=self.send(data)
         return res
